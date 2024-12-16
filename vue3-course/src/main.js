389b18ef516ee21a74b2c7bb09d6c10401e0df9e
Anton Tsyhan <anton.tsyhan@uco.co.il>
2024-12-16 20:07:19 +0300

    add first type

--- vue3-course/src/main.js
+++ vue3-course/src/main.js
@@ -1,11 +1,10 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+createApp(App).mount('#app');
 
-const app = createApp(App)
+// const app = createApp(App);
 
-app.use(router)
+// app.use(router);
 
-app.mount('#app')
+// app.mount('#app');
